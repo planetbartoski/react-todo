d65ff07b4323166d089d810be4f2c28787ff2500
Bartosz Moszczynski <bart.moszczynski@gmail.com>
2016-06-02 11:52:53 +0100

    styles refactoring

--- src/components/TodoPage.js
+++ src/components/TodoPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import AddTodo from '../containers/AddTodo';
 import VisibleTodoList from '../containers/VisibleTodoList';
 import TodoSelector from './TodoSelector';
-import '../styles/todo.scss';
+import '../styles/todoPage.scss';
 
 /**
  * TodoPage Component, renders the Todo app on the screen 
